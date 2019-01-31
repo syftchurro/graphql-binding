9b550da3ad1f13e7baeec89f2f6b2a3ceb66ae76
Mostafa Sholkamy <shalkamm@gmail.com>
2019-01-31 23:31:08 +0200

    added remote binding using uri

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const binding = require('./binding')
+const remoteBinding = require('./remoteBinding')
 
-module.exports = binding
+module.exports = { binding, remoteBinding }
---
+++ remoteBinding.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch')
+const { HttpLink } = require('apollo-link-http')
+const { introspectSchema } = require('graphql-tools')
+const Binding = require('./graphql-binding')
+
+const remoteBinding = uri => {
+  const link = new HttpLink({
+    uri,
+    fetch
+  })
+  return introspectSchema(link)
+    .then(typeDefs => {
+      return new Binding({
+        endpoint: uri,
+        typeDefs
+      })
+    })
+    .catch(err => console.error(err))
+}
+
+module.exports = remoteBinding
